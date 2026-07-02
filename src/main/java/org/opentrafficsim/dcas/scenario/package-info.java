5696e9284bcedd40bece4d7e536bf47794ad0bef
WJSchakel <w.j.schakel@tudelft.nl>
2026-07-02 19:04:12 +0200

    Initial project setup.

---
+++ src/main/java/org/opentrafficsim/dcas/scenario/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains scenario definition files.
+ * <p>
+ * Copyright (c) 2026-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.<br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * @author Wouter Schakel
+ * @author Saeed Rahmani
+ */
+package org.opentrafficsim.dcas.scenario;
---
+++ src/main/java/org/opentrafficsim/dcas/tactical/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains tactical models that define driver behavior with Driver Control Assistance System (DCAS).
+ * <p>
+ * Copyright (c) 2026-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.<br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * @author Wouter Schakel
+ * @author Saeed Rahmani
+ */
+package org.opentrafficsim.dcas.tactical;
