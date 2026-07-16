6e7559a5560b3ab97d6d499dcb67210074c48086
Saeed Rahmani <s.rahmani@tudelft.nl>
2026-07-16 12:28:23 +0200

    this is Saeed's first commit.

---
+++ src/main/java/org/opentrafficsim/dcas/tactical/MyTest.java
@@ -0,0 +1,17 @@
+package org.opentrafficsim.dcas.tactical;
+
+/**
+ * MyTest.java.
+ * <p>
+ * Copyright (c) 2026-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.<br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * @author Wouter Schakel
+ * @author Saeed Rahmani
+ */
+public class MyTest
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("my first program...");
+    }
+}
